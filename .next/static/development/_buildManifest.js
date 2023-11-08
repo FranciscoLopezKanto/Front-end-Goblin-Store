db3fc1658cf2e2a15f94c073f7d99b1356608f2b
FranciscoLopezKanto <franciscolopmez599@gmail.com>
2023-11-08 12:57:52 -0400

    Update
    mejoras de problemas de tamaño del repositorio

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/filter":["static\u002Fchunks\u002Fpages\u002Ffilter.js"],sortedPages:["\u002F_app","\u002F_error","\u002Ffilter"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
